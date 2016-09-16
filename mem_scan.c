115305df999b82daf9ff5d130632f5d3ed6a24a4
Brett Commandeur <bcommandeur@gmail.com>
2016-09-16 13:09:23 -0600

    rename function for asn

--- mem_scan.c
+++ mem_scan.c
@@ -6,7 +6,10 @@
 #include <setjmp.h>
 #include "mem_scan.h"
 
-static const int PAGE_SIZE = 4096, 
+static const unsigned int 
+	PAGE_SIZE = 0x400,
+	MIN_SIZE = 2, 
+	MAX_SIZE = 65536,
 	MIN_ADDR = 0x0, 
 	MAX_ADDR = 0xFFFFFFFF;
 
@@ -17,7 +20,7 @@ void handle_segv(int sig_id)
 	longjmp(env, 1);
 }
 
-int get_access(char * addr)
+int get_mem_mode(char * addr)
 {
 	struct sigaction act, oldact;
 	int mem_mode = MEM_NO; // assume we can't access address
@@ -33,13 +36,13 @@ int get_access(char * addr)
 	if (!setjmp(env)) {
 		read_data = *addr;
 		if (read_data) {
-			printf("Read from %p\n", addr);
+			// printf("Read from %p\n", addr);
 			mem_mode = MEM_RO;
 		}
 	}
 	if (!setjmp(env)) {
 		*addr = write_data;
-		printf("Wrote to %p\n", addr);
+		// printf("Wrote to %p\n", addr);
 		mem_mode = MEM_RW;
 	}
 		
@@ -50,18 +53,15 @@ int get_access(char * addr)
 	return mem_mode;
 }
 
+/*
 void scan_memory(void)
 {
 	//int mem_mode;
+
 	char *curr_addr = (char *) MIN_ADDR;
 
 	while (curr_addr <= (char *) MAX_ADDR) {
-		/*
-		mem_mode = get_access(curr_addr);
-		if (mem_mode != MEM_NO)
-			printf("Accessed %p\n", curr_addr);
-		*/	
-		get_access(curr_addr);
+		get_mem_mode(curr_addr);
 		if (curr_addr + PAGE_SIZE < curr_addr)
 			//overflow detected
 			break;
@@ -70,8 +70,24 @@ void scan_memory(void)
 
 }
 
+*/
+
 int get_mem_layout (struct memregion *regions, unsigned int size)
 {
+	int mem_mode;
+	//unsigned int region;
+	char *curr_addr = (char *) MIN_ADDR;
+
+	while (curr_addr <= (char *) MAX_ADDR) {
+		mem_mode = get_mem_mode(curr_addr);
+		if (mem_mode != MEM_NO)
+			printf("Accessed %p\n", curr_addr);
+		if (curr_addr + PAGE_SIZE < curr_addr)
+			//overflow detected
+			break;
+		curr_addr += PAGE_SIZE;
+	}
+
 	return 0;
 }
 
@@ -84,7 +100,12 @@ int get_mem_diff (struct memregion *regions, unsigned int howmany,
 
 int main(void)
 {
-	scan_memory();
+	unsigned int size = MAX_SIZE;
+	struct memregion regions[size];
+
+	get_mem_layout(regions, size);
+
+	// scan_memory();
 
 	return 0;
 }
