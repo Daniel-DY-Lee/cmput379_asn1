b31a59a477fbb417aa8d82bd9a9cae908791826e
Brett Commandeur <bcommandeur@gmail.com>
2016-09-16 19:30:12 -0600

    fix '1' vs 49 bug

--- mem_scan.c
+++ mem_scan.c
@@ -9,7 +9,7 @@
 static const unsigned int 
 	PAGE_SIZE = 0x400,
 	MIN_SIZE = 2, 
-	MAX_SIZE = 65537,
+	MAX_SIZE = 65536,
 	MIN_ADDR = 0x0, 
 	MAX_ADDR = 0xFFFFFFFF;
 
@@ -20,17 +20,17 @@ void handle_segv(int sig_id)
 	longjmp(env, 1);
 }
 
-char get_mem_mode(void *curr_addr)
+int get_mem_mode(void *curr_addr)
 {
-	struct sigaction act, oldact;
-	char mem_mode = MEM_NO; 		// assume r/w mode then test
-	char read_data, write_data = '1';
-	char *addr = (char *) curr_addr;
+	struct sigaction act, oldact;		// signal handler items
+	int mem_mode = MEM_NO; 			// assume r/w mode then test
+	char read_data, write_data = 1;		// r/w checkers
+	char *addr = (char *) curr_addr;	// current address
 
 	// Setup custom SIGSEGV handler
 	act.sa_handler = handle_segv;
 	sigemptyset(&act.sa_mask);
-	act.sa_flags = SA_NODEFER;		// TODO: why? block signals
+	act.sa_flags = SA_NODEFER;		// TODO: NEEDED!!! But why?
 	sigaction(SIGSEGV, &act, &oldact);
 	
 	// Get read/write mode of given address
@@ -56,63 +56,55 @@ char get_mem_mode(void *curr_addr)
 int get_mem_layout (struct memregion *regions, unsigned int size)
 {
 	unsigned char curr_mem_mode;		// current r/w mode
-	int r_i = 0;				// current region index
+	unsigned int r_i = 0;			// current region index
 	void *max_addr = (void *) MAX_ADDR;	// max address
 	void *curr_addr = (void *) MIN_ADDR;	// current address
 
 	// Setup the first region
 	regions[r_i].from = curr_addr;
-	regions[r_i].mode = get_mem_mode(curr_addr);
+	regions[r_i].mode = (unsigned char) get_mem_mode(curr_addr);
 	curr_addr += PAGE_SIZE;
 
-	// Iterate through address space starting after first address
+	// Iterate through address space starting after first address.
 	while (curr_addr <= max_addr) {
 		
-		// Get r/w  mode of current address
-		curr_mem_mode = get_mem_mode(curr_addr);
-
-		/* TODO: TEST 
-		if (!regions[r_i].mode) {
-			// (No r/w mode for current region)
-			
-			// Note current r/w mode
-			regions[r_i].mode = curr_mem_mode;
-
-		} else ...
-		*/
+		// Get r/w  mode of current address.
+		curr_mem_mode = (unsigned char) get_mem_mode(curr_addr);
 		
+		// Look for new r/w mode and therefore new region.
 		if (regions[r_i].mode != curr_mem_mode) {
-			// (New r/w mode found, therefore new region)
+			// (New region found).
 
 			// Set final address of current region.
-			regions[r_i].to = curr_addr - PAGE_SIZE;
-			printf("Region found: %p --> %p, %d\n", 
-				regions[r_i].from,
-				regions[r_i].to,
-				regions[r_i].mode);
+			if (r_i <= size) 
+				regions[r_i].to = curr_addr - PAGE_SIZE;
 
-			// Move to next region (if available)
-			// and set first address and r/w mode of it.
-			if (++r_i > size) break;
-			regions[r_i].from = curr_addr;
-			regions[r_i].mode = curr_mem_mode;
+			// Move to next region (if size permits)
+			// and set first address and r/w mode of it
+			if (++r_i <= size) {
+				regions[r_i].from = curr_addr;
+				regions[r_i].mode = curr_mem_mode;
+			}
 		}
 
-		// Ensure increment address without overflow
+		// See if next increment causes overflow.
 		if (curr_addr + PAGE_SIZE <= curr_addr) {
-			// (Last possible address)
+			// (Last possible address).
 
 			// Set final address of last region.
-			regions[r_i].to = curr_addr;
+			if (r_i <= size)
+				regions[r_i].to = curr_addr;
 
-			// Exit loop before overflow
+			// Exit loop before overflow.
 			break;
 		}
+
+		// Safe to increment
 		curr_addr += PAGE_SIZE;
 		
 	}
 
-	return 0;
+	return r_i + 1; // size is one more than max index
 }
 
 int get_mem_diff (struct memregion *regions, unsigned int howmany,
@@ -124,12 +116,36 @@ int get_mem_diff (struct memregion *regions, unsigned int howmany,
 
 int main(void)
 {
-	unsigned int size = MAX_SIZE;
+
+	// Setup our function and tests
+	unsigned int r_i, ret_size, size = MAX_SIZE;
 	struct memregion regions[size];
+	
+	// Run our function
+	ret_size = get_mem_layout(regions, size);
 
-	get_mem_layout(regions, size);
-
-	// scan_memory();
+	// Test output of our function
+	for (r_i = 0; r_i < ret_size; r_i++) {
+		printf("Region %d: %p --> %p, mode: ", 
+			r_i,
+			regions[r_i].from,
+			regions[r_i].to);
+		
+		switch(regions[r_i].mode) {
+			case MEM_RO:
+				printf("RO\n");
+				break;
+			case MEM_RW:
+				printf("RW\n");
+				break;
+			case MEM_NO:
+				printf("NO\n");
+				break;
+			default: 
+				printf("Unknown\n");
+				break;
+		}
+	}
 
 	return 0;
 }
